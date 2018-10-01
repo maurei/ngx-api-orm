bc9f4f69d9a6b9b9aeb53f96fd0fa51ff3ea087d
Maurits Moeys <maurits.moeys@gmail.com>
2018-10-01 12:46:01 +0200

    fix: removed dist

--- core/dist/ngx-api-orm-core.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { RelationConfiguration as ɵc } from './src/relations/relation-configuration';
-export { RawInstance as ɵa } from './src/resource.core';
-export { ResourceType as ɵb } from './src/utils';
